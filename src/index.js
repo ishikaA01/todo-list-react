4ef4c7b0a4482ab7084bc23a9677ad6b3d7b7f72
ishikaA01 <112720764+ishikaA01@users.noreply.github.com>
2023-12-10 13:04:34 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// Filename - src/index.js:
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// This is a functional component
+const Welcome = () => {
+	return <h1>Hello World!</h1>;
+};
+
+ReactDOM.render(
+	<Welcome />,
+	document.getElementById("root")
+);
